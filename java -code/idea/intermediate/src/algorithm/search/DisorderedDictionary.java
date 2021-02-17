de5be93fab8a09bc550c4c8ef9ad29667f1de49c
zjh <201816040313@stu.haut.edu.cn>
2021-02-17 09:42:25 +0800

    add search files!

---
+++ java -code/idea/intermediate/src/algorithm/search/DisorderedDictionary.java
@@ -0,0 +1,148 @@
+package algorithm.search;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class DisorderedDictionary<Key,Value>{
+    private Node first;
+    private int count=0;
+    public DisorderedDictionary(){}
+    private class Node{
+        private Key key;
+        private Value value;
+        private Node next;
+        Node(Key key,Value value,Node next){
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+    public void put(Key key,Value value){
+        Node temp = this.first;
+        for (Node x=first;x!=null;x=x.next){
+            if (x.key.equals(key)){
+                x.value=value;
+                return;
+            }
+        }
+        this.first = new Node(key,value,temp);
+        this.count++;
+    }
+    public Value get(Key key){
+        for (Node x=first;x!=null;x=x.next){
+            if (x.key.equals(key)){
+                return x.value;
+            }
+        }
+        return null;
+    }
+    public int size(){return this.count;}
+    public boolean isEmpty(){return this.size()==0;}
+    public Value remove(Key key){
+        Node currentNode = this.first;
+        Node temp = this.first.next;
+        if (this.size()==1&&currentNode.key.equals(key)){
+            this.first=null;
+            this.count--;
+            return this.first.value;
+        }else if (this.size()>1){
+            if (currentNode.key.equals(key)){
+                this.first = this.first.next;
+                this.count--;
+                return this.first.value;
+            }else {
+                while (temp!=null){
+                    if (temp.key.equals(key)){
+                        if (temp.next==null){
+                            currentNode.next=null;
+                        }else{
+                            currentNode.next=temp.next;
+                        }
+                        this.count--;
+                        return temp.value;
+                    }else {
+                        currentNode=currentNode.next;
+                        temp=temp.next;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    public void toDict(){
+        String str = "";
+        for (Node x=this.first;x!=null;x=x.next){
+            str+=x.key+":"+x.value+",";
+        }
+        if (str.equals("")){
+            System.out.println("{"+"}");
+        }else System.out.println("{"+str.substring(0,str.length()-1)+"}");
+    }
+    public void toListKey(){
+        String s = "";
+        for (Node x=first;x!=null;x=x.next){
+            s+=x.key+",";
+        }
+        if (s.equals("")) System.out.println("["+"]");
+        else System.out.println("["+s.substring(0,s.length()-1)+"]");
+    }
+    public void toListValue(){
+        String s = "";
+        for (Node x=first;x!=null;x=x.next){
+            s+=x.value+",";
+        }
+        if (s.equals("")) System.out.println("["+"]");
+        else System.out.println("["+s.substring(0,s.length()-1)+"]");
+    }
+    public boolean contains(Key key){
+        for (Node x=first;x!=null;x=x.next){
+            if (x.key.equals(key)) return true;
+        }
+        return false;
+    }
+    public Iterable<Key> keys(){
+        Iterable<Key> iterable = new Iterable<Key>() {
+            @Override
+            public Iterator<Key> iterator() {
+                return new DictionaryIterator();
+            }
+            class DictionaryIterator implements Iterator<Key>{
+                private int i = count;
+                @Override
+                public boolean hasNext() {
+                    return this.i>0;
+                }
+                private Key iter(){
+                    Node temp = first;
+                    Key key = temp.key;
+                    if (temp.next!=null) temp=temp.next;
+                    return key;
+                }
+                @Override
+                public Key next() {
+                    Key key = iter();
+                    this.i--;
+                    return key;
+                }
+            }
+        };
+        return iterable;
+    }
+    public static void main(String[] args) {
+        DisorderedDictionary<Integer,String> dictionary = new DisorderedDictionary<>();
+        dictionary.put(1,"zr");
+        dictionary.put(2,"zjh");
+        dictionary.put(3,"zx");
+        dictionary.put(4,"gfc");
+        dictionary.put(5,"lwx");
+        dictionary.put(6,"lt");
+        dictionary.toDict();
+        System.out.println(dictionary.remove(4));
+        dictionary.toDict();
+        dictionary.toListKey();
+        dictionary.put(7,"lwy");
+        dictionary.toDict();
+        dictionary.toListKey();
+    }
+}
---
+++ java -code/idea/intermediate/src/algorithm/search/ListNode.java
@@ -0,0 +1,137 @@
+package algorithm.search;
+
+import java.util.Iterator;
+
+public class ListNode<T> implements Iterable<T> {
+    private Node head;
+    private int count=0;
+    public ListNode(){}
+    public void add(T value){
+        Node temp = this.head;
+        this.head = new Node(value,temp);
+        this.count++;
+    }
+    public boolean remove(T value){
+        Node currentNode = this.head;
+        Node temp = this.head.next;
+        if (this.count==1&&currentNode.value.equals(value)){
+            head = null;
+            this.count--;
+            return true;
+        }else if (this.count>1){
+            if (currentNode.value.equals(value)){
+                head = head.next;
+                this.count--;
+                return true;
+            }else {
+                while (temp!=null){
+                    if (temp.value.equals(value)){
+                        if (temp.next==null){
+                            currentNode.next = null;
+                        }else {
+                            currentNode.next=temp.next;
+                        }
+                        this.count--;
+                        return true;
+                    }else {
+                        currentNode = currentNode.next;
+                        temp = temp.next;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public T get(int index){
+        T value = null;
+        if (index>=this.size()||index<0) throw new RuntimeException("下标越界!");
+        int temp=0;
+        for (Node x=this.head;x!=null;x=x.next){
+            if (temp==index){
+                value = x.value;
+                break;
+            }
+            temp++;
+        }
+        return value;
+    }
+    public void set(int index,T value){
+        if (index>=this.size()||index<0) throw new RuntimeException("下标越界!");
+        int temp=0;
+        for (Node x=this.head;x!=null;x=x.next){
+            if (temp==index){
+                x.value = value;
+                break;
+            }
+            temp++;
+        }
+    }
+    public void toArray(){
+        String str = "";
+        for (Node x=this.head;x!=null;x=x.next){
+            str+=x.value+",";
+        }
+        if (str.equals("")) System.out.println("[]");
+        else System.out.println("["+str.substring(0,str.length()-1)+"]");
+    }
+    public boolean contains(T value){
+        for (Node x=this.head;x!=null;x=x.next){
+            if (x.value.equals(value)){
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public Iterator<T> iterator() {
+        return new ListNodeIterator();
+    }
+    public int size(){return this.count;}
+    public boolean isEmpty(){return this.size()==0;}
+    private class ListNodeIterator implements Iterator<T>{
+        private int i = count;
+        @Override
+        public boolean hasNext() {
+            return this.i>0;
+        }
+        private T iter(){
+            Node tem = head;
+            T value = tem.value;
+            if (tem.next!=null){
+                tem=tem.next;
+            }
+            return value;
+        }
+        @Override
+        public T next() {
+            T val = this.iter();
+            this.i--;
+            return val;
+        }
+    }
+    private class Node{
+        private T value;
+        private Node next;
+        Node(T value,Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode<Integer> listNode = new ListNode<>();
+        listNode.add(1);
+        listNode.add(7);
+        listNode.add(3);
+        listNode.add(8);
+        listNode.add(4);
+        listNode.add(6);
+        listNode.add(2);
+        listNode.toArray();
+        System.out.println(listNode.get(2));
+        System.out.println(listNode.get(0));
+        listNode.set(2,5);
+        System.out.println(listNode.get(0));
+        listNode.toArray();
+    }
+}
